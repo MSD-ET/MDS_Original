fdf375fae151f9554bb1a26e74cef5b92a9d630b
user <user@this.com>
2020-10-08 08:55:37 -0400

    Added Customer Api

---
+++ customer-api/src/main/java/com/bah/customerapi/CustomerApiApplication.java
@@ -0,0 +1,14 @@
+package com.bah.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiApplication.class, args);
+	}
+
+	
+}
---
+++ customer-api/src/main/java/com/bah/customerapi/domain/Customer.java
@@ -0,0 +1,43 @@
+package com.bah.customerapi.domain;
+
+public class Customer {
+
+String password;
+String email;
+long id;
+String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
+
